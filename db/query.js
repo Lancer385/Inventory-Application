266f4f55b635d23b99cf986982d07dfe03aaafe8
Lancer385 <ahmedkhalaf1234123@gmail.com>
2026-06-13 16:35:41 +0300

    feat: add query for getting games with their category - added a function that combine multiple queries

--- db/query.js
+++ db/query.js
@@ -1,16 +1,24 @@
-const { Pool } = require('./pool');
+const pool  = require('./pool');
 
 async function getAllGames(){
-    const { rows } = await pool.query("SELECT * FROM games");
-    return rows;
-}
+    const games_platforms = (await pool.query('SELECT games.name AS "gameName" ,games.description AS "gameDescription",platforms.name AS platforms FROM games JOIN games_platforms ON games.id = games_platforms.game_id JOIN platforms ON platforms.id = platform_id')).rows;
+    const games_genres = (await pool.query('SELECT games.name AS "gameName" ,games.description AS "gameDescription",genres.types AS genres FROM games JOIN games_genres ON games.id = games_genres.game_id JOIN genres ON genres.id = genre_id')).rows;
+    const games_developers = (await pool.query('SELECT games.name AS "gameName" ,games.description AS "gameDescription",developers.name AS developers FROM games JOIN games_developers ON games.id = games_developers.game_id JOIN developers ON developers.id = developer_id')).rows;
+    games_platforms.push(...games_genres)
+    games_platforms.push(...games_developers)
+    const result = combine(games_platforms, "genres", "platforms", "developers");
+    return result;
+};
+
+
+
 
 async function addNewGame(name, description = ""){
     await pool.query("INSERT INTO games (name, description) VALUES ($1, $2)", [name, description]);
-}
+};
 
 async function addNewCategory(tableName, column, name){
-    await pool.query(`INSERT INTO ${tableName} (${column}) VALUES ($1)`, [name])
+    await pool.query(`INSERT INTO ${tableName} (${column}) VALUES ($1)`, [name]);
 };
 
 async function addRelation(junctionName, gameName, category){
@@ -21,5 +29,33 @@ async function addRelation(junctionName, gameName, category){
             (SELECT id FROM ${category.tableName} WHERE ${category.column} = $2)
             )
         `
-    ,[gameName, category.value])
+    ,[gameName, category.value]);
+};
+
+getAllGames();
+
+
+
+
+function combine(arr, genres, platforms, developers) {
+        const result = arr.reduce((acc, currentItem) => {
+        const condition = acc.find(item => item.gameName === currentItem.gameName );
+        if (!condition) {
+        const newCurr = {
+            gameName: currentItem.gameName,
+            gameDescription: currentItem.gameDescription,
+            [platforms]: [currentItem[platforms]],
+            [genres]: [],
+            [developers]: []
+        }
+        return acc.concat([newCurr])
+        } else {
+            currentItem[genres] && condition[genres].push(currentItem[genres]);
+            currentItem[platforms] && condition[platforms].push(currentItem[platforms]);
+            currentItem[developers] && condition[developers].push(currentItem[developers]);
+            return acc;
+        }
+    }, [])
+    return result;
 }
+
