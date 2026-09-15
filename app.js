d6d76cf3e5404270070c9afff0a1064ac26970fb
Lancer385 <ahmedkhalaf1234123@gmail.com>
2026-09-15 12:46:04 +0300

    style: add pixel art style to the page - added pressStart2p font - added footer to link the project source code - applied css to the index page - added ejs file extensions for livereload to refresh on change - fix missing to replace old partial script file with public file in gamesForm

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ const categoriesRouter = require("./routes/CategoriesRouter");
 const CustomError = require("./utils/customError");
 const PORT = process.env.PORT || 8000;
 const assetsPath = path.join(__dirname, "public");
-
+const viewsPath = path.join(__dirname, "views");
 if (process.env.NODE_ENV !== "production") {
   const livereload = require("livereload");
   const connectLiveReload = require("connect-livereload");
 
-  const liveReloadServer = livereload.createServer();
-  liveReloadServer.watch(assetsPath);
+  const liveReloadServer = livereload.createServer({
+    exts: ["css", "js", "ejs"],
+  });
+  liveReloadServer.watch([assetsPath, viewsPath]);
   app.use(connectLiveReload());
 }
-app.set("views", path.join(__dirname, "views"));
+app.set("views", viewsPath);
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
